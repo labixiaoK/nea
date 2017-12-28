315533eb909cccaf309b866aa595163d87927e17
labixiaoK <shaokelv@163.com>
2017-12-28 17:35:17 +0800

    Update train_nea.py

--- train_nea.py
+++ train_nea.py
@@ -203,7 +203,7 @@ total_eval_time = 0
 
 for ii in range(args.epochs):
 	# Training
-	t0 = time()
+	t0 = time()						#nb_epoch??
 	train_history = model.fit(train_x, train_y, batch_size=args.batch_size, nb_epoch=1, verbose=0)
 	tr_time = time() - t0
 	total_train_time += tr_time
